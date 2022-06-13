5166c184ec5549802931ae73b0ab71da85ea101d
Jagoda Tata <sc20jmt@leeds.ac.uk>
2022-06-13 23:16:53 +0200

    Task 6 - removing redundant tests

--- src/jtata/tasks/MainTest.java
+++ src/jtata/tasks/MainTest.java
@@ -6,46 +6,7 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     Main tests = new Main();
-
-    @Test
-    void add() throws Exception {
-
-        // Task 1
-        assertEquals(23, tests.Add("11,12"));
-        assertEquals(0, tests.Add(""));
-        assertEquals(123, tests.Add("123"));
-        assertEquals(136, tests.Add("123,12,1"));
-
-        // Task 2
-        assertEquals(142, tests.Add("123,12,1,2,4"));
-        assertEquals(54, tests.Add("1,1,2,3,5,8,13,21"));
-        assertEquals(13, tests.Add("2,1,3,7"));
-
-
-        // Task 3
-        assertEquals(13, tests.Add("2\n1,3,7"));
-        assertEquals(-1, tests.Add("1,1,,"));
-        assertEquals(-1, tests.Add("1\n,"));
-        assertEquals(8, tests.Add("1\n3,4"));
-        assertEquals(-1, tests.Add("1\n3,4,,"));
-        assertEquals(-1, tests.Add(",,"));
-
-        // Task 4
-        assertEquals(39, tests.Add("//-\n1-2-34-2"));
-        assertEquals(-1, tests.Add("//-\n1-2+34-2"));
-        assertEquals(-1, tests.Add("//\n1-2-34-2"));
-        assertEquals(39, tests.Add("///\n1/2/34/2"));
-        assertEquals(-1, tests.Add("///\n1/2\n34,2"));
-        assertEquals(-1, tests.Add("///\n"));
-        assertEquals(1, tests.Add("///\n1"));
-        assertEquals(-1, tests.Add("/3\n13532"));
-        assertEquals(-1, tests.Add("//313532"));
-        assertEquals(-1, tests.Add("//-31-35-32"));
-
-        // Task 5
-
-
-    }
+    
 
     @Test
     void addTask1() throws Exception {
